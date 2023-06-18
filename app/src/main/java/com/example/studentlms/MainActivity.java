71b4b0f55a7d5d6fb95bcf9000155d4d2562af69
junaid786-prog <junaidshoaib555@gmail.com>
2023-06-18 14:48:09 +0500

    main and add student activity xml updated

--- app/src/main/java/com/example/studentlms/MainActivity.java
+++ app/src/main/java/com/example/studentlms/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.studentlms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addStudent = findViewById(R.id.add_student);
+        addStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent moveActivity = new Intent(MainActivity.this, AddStudent.class);
+                startActivity(moveActivity);
+            }
+        });
     }
 }
